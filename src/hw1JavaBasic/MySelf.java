fbe6011e582e23ddf1d76a7e06de1598685c359e
MD Rasel Al MAHMUD <rasel@MDnoMacBook-Pro.local>
2023-05-24 19:41:31 -0400

    homework 1

---
+++ src/hw1JavaBasic/MySelf.java
@@ -0,0 +1,26 @@
+package hw1JavaBasic;
+
+/*
+- Project: File -- New -- Java Project -- Name starts with Upper Case.
+- Package: Expand the Project name and then select src file --> right click --> New --> select package (yellow 4 box)  --> Name starts with lowercase 
+- Class: select package --> right click --> New --> Select Class (Green circular) --> Name starts with UpperCase --> Select main method
+
+ */
+
+//Entry point for Java. 
+// is a static method
+//actually the method name is main
+//represented by
+//public static void main(String[] args) {
+//}
+
+public class MySelf {
+
+	public static void main(String[] args) {
+
+		System.out.println("My Name is Jahanara Taslim");
+		System.out.println("My Address is 4900 battery lane, Bethesda,Maryland");
+
+	}
+
+}
---
+++ src/hw2JavaBasics/MyCar.java
@@ -0,0 +1,10 @@
+package hw2JavaBasics;
+
+public class MyCar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
